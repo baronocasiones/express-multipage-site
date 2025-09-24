221639f6c7459497ee42a35f6a0a5ee107b003b2
Baron <baronocasiones@gmail.com>
2025-09-24 11:49:02 +0800

    added challenge task

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
@@ -18,6 +20,12 @@ app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'contact.html'));
 });
 
+app.get('/posts', (req, res) => {
+    const postsData = require('./data/posts.json');
+    res.render('blog', {posts: postsData});
+});
+
 app.listen(PORT, () => {
     console.log(`Server running at https://localhost:${PORT}`)
 });
+
